b3833820cc2c116872e15cf6e011fd33ae20e45f
bannu773 <bandibannu773@gmail.com>
2025-06-03 09:25:13 +0530

    started listener

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -4,10 +4,7 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3Client = new S3Client({
   region: process.env.REGION,
-  credentials: {
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  },
+
 });
 
 export const uploadToS3 = async (file, fileName) => {
